493be6dfd9f6f44146563a59db9a287cb8e3cf51
Eduardo Azuolas <eduardoazuolas@gmail.com>
2021-09-27 16:00:38 -0300

    Configurando Modal, dando funcionalidade e estilizando os botões e configurando a API

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [ isNewTransactionModalOpen, setIsNewMTransactionModalOpen ] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewMTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewMTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
